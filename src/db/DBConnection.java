fd503bf721e6b48e72e8b101357d4ab1c690c949
Cristian <cjalbarracinb@udistrital.edu.co>
2026-05-22 18:17:53 -0500

    Update database connection details for DBConnection

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "";
-    private static final String USER = "";
+    private static final String URL = "jdbc:postgresql://neondb_owner:npg_Om2ocUqjXE4C@ep-restless-breeze-aptskfly-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+    private static final String USER = "neondb_owner";
     private static final String PASS = "";
 
     public static Connection getConnection() throws SQLException {
