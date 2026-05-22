f86f5ab3a6f1b6ae4e1cbb98d710f7617b49a02a
Cristian <cjalbarracinb@udistrital.edu.co>
2026-05-22 18:20:24 -0500

    Change database password in DBConnection.java
    Updated database connection password.

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:postgresql://neondb_owner:npg_Om2ocUqjXE4C@ep-restless-breeze-aptskfly-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
     private static final String USER = "neondb_owner";
-    private static final String PASS = "";
+    private static final String PASS = "npg_Om2ocUqjXE4C";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
