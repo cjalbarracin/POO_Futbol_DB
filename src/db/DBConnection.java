80b1ca572b1eb205023ebc0285c8c4178eb73c79
usuario <cjalbarracin@udistrital.edu.co>
2026-05-22 22:39:52 -0500

    11

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -6,14 +6,13 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:postgresql://neondb_owner:npg_Om2ocUqjXE4C@ep-restless-breeze-aptskfly-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+    // URL limpia: sin usuario ni contraseña dentro
+    private static final String URL = "jdbc:postgresql://ep-restless-breeze-aptskfly-pooler.c-7.us-east-1.aws.neon.tech:5432/neondb?sslmode=require";
     private static final String USER = "neondb_owner";
     private static final String PASS = "npg_Om2ocUqjXE4C";
 
     public static Connection getConnection() throws SQLException {
+        // El Driver se encarga de combinar estos tres datos correctamente
         return DriverManager.getConnection(URL, USER, PASS);
     }
-
-    //
-
 }
