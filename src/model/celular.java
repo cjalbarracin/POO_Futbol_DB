90c0f91428119c7910f2b5abcc4a713227ada15f
usuario <cjalbarracin@udistrital.edu.co>
2026-05-22 23:11:02 -0500

    11

--- src/model/celular.java
+++ src/model/celular.java
@@ -1,27 +1,12 @@
 package model;
 
-import java.sql.Timestamp;
-
 public class celular {
-
-    private int id;
     private String marca;
     private String modelo;
     private int camara;
     private int bateria;
-    private Timestamp creado;
 
-
-    public celular(int id, String marca, String modelo, int camara, int bateria, Timestamp creado) {
-        this.id = id;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.camara = camara;
-        this.bateria = bateria;
-        this.creado = creado;
-    }
-
-
+    // Constructor
     public celular(String marca, String modelo, int camara, int bateria) {
         this.marca = marca;
         this.modelo = modelo;
@@ -29,28 +14,20 @@ public class celular {
         this.bateria = bateria;
     }
 
-
-    public int getId() {
-        return id;
-    }
-
-    public String getmarca() {
+    // --- ESTOS SON LOS MÉTODOS QUE TE FALTAN Y CAUSAN EL ERROR ---
+    public String getMarca() {
         return marca;
     }
 
-    public String getmodelo() {
+    public String getModelo() {
         return modelo;
     }
 
-    public int getcamara() {
+    public int getCamara() {
         return camara;
     }
 
-    public int getbateria() {
+    public int getBateria() {
         return bateria;
-    }
-
-    public Timestamp getCreado() {
-        return creado;
     }
 }
