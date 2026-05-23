de5314b4a68d450a5401e4dc8c952b6dd4f9430f
usuario <cjalbarracin@udistrital.edu.co>
2026-05-22 19:23:08 -0500

    8

--- src/db/operaciones/ReadJugadores.java
+++ src/db/operaciones/ReadJugadores.java
@@ -1,1 +1,32 @@
+package db.operaciones;
+import db.DBConnection;
+import java.sql.*;
 
+public class ReadCelulares {
+
+    public static void main(String[] args) {
+
+        String read_query = "SELECT * FROM celulares";
+
+        try( Connection conn = DBConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(read_query)
+        )
+        {
+            System.out.println("ID | MARCA | MODELO | CREADO");
+            while (rs.next())
+            {
+                int id = rs.getInt("id");
+                String marca = rs.getString("marca");
+                String modelo = rs.getString("modelo");
+                Timestamp creado = rs.getTimestamp("creado");
+
+                System.out.println(id + " | " + marca + " | " + modelo + " | " + creado);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+}
