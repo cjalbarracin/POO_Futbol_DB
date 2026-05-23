99c86114bd769eeaf0a66b6aafdc93188ec5b3ef
usuario <cjalbarracin@udistrital.edu.co>
2026-05-22 23:41:58 -0500

    66

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -1,18 +1,28 @@
 package db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
+    private static Properties prop = new Properties();
 
-    // URL limpia: sin usuario ni contraseña dentro
-    private static final String URL = "jdbc:postgresql://ep-restless-breeze-aptskfly-pooler.c-7.us-east-1.aws.neon.tech:5432/neondb?sslmode=require";
-    private static final String USER = "neondb_owner";
-    private static final String PASS = "npg_Om2ocUqjXE4C";
+    static {
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            prop.load(input);
+        } catch (IOException ex) {
+            System.err.println("Error: No se encuentra config.properties");
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        // El Driver se encarga de combinar estos tres datos correctamente
-        return DriverManager.getConnection(URL, USER, PASS);
+        return DriverManager.getConnection(
+                prop.getProperty("db.url"),
+                prop.getProperty("db.user"),
+                prop.getProperty("db.pass")
+        );
     }
 }
